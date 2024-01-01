e4de21883afb656b8bbd46f67b70a269dbc1cf5a
Yogeshwar Khurmi <74640546+ykverma99@users.noreply.github.com>
2024-01-01 17:58:41 +0530

    Getting the qus data from the api

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
-      // TODO 1: Declare & assign variables pointing to the corresponding element(s)
-      // statement should be the "statement" div
-      const statement = document.getElementById("statement");
-      // optionButtons should be all the elements within the "options" div
-      const optionButtons = document.querySelector("#options").children;
-      // explanation should be the "explanation" div
-      const explanation = document.getElementById("explanation");
+async function fetchData() {
+  const res = await fetch("https://8kz9z.wiremockapi.cloud/data");
+  const data = await res.json();
+  return data;
+}
 
-      // TODO 2: Declare & assign a variable called fact
-      // Its value should be an object with a statement, true/false answer, and explanation
-      const fact = {
-        statement: "Arrays are like objects",
-        answer: true,
-        explanation: "Arrays are a kind of object with special properties",
-      };
+// const data = await fetchData()
+// console.log(data)
+let GLOBALDATA;
+let counter = -1
 
-      // TODO 3: Set the text of the statement element to the fact's statement
+const totalQus = document.getElementById("total_qus")
+const rightQus = document.getElementById("right") 
+const wrongQus = document.getElementById("wrong") 
+const statement = document.getElementById("statement")
+const optionBtn = document.querySelector("#options").children
+const startBtn = document.getElementById("next")
+let right = 0
+let wrong = 0
 
-      statement.textContent = fact.statement;
+function startGame(curr,length,statementText){
+    wrongQus.textContent = wrong
+    rightQus.textContent = right
+    statement.textContent = statementText
+    totalQus.textContent = `${curr}/${length}`
+    for(let button of optionBtn){
+        button.removeAttribute("disabled")
+        button.classList.remove("correct")
+        button.classList.remove("incorrect")
+    }
+    startBtn.textContent = "Next Qus"
+}
 
-      // TODO 4: Declare disable & enable functions to set or remove the "disabled" attribute from a given button element
-      // disable(button) should set the button element's attribute "disabled" to the value ""
-      const disable = (button) => button.setAttribute("disabled", "");
+function isCorrect(guess){
+    for(let button of optionBtn){
+        button.addEventListener('click',()=>{
+            
+            for(let disabledButton of optionBtn){
+                disabledButton.setAttribute("disabled","true")
+            }
 
-      // enable(button) should remove the attribute "disabled" from the button element
-      const enable = (button) => button.removeAttribute("disabled");
+            if(button.value === guess.toLowerCase()){
+                right++
+                button.classList.add("correct")
+            }else{
+                wrong++
+                button.classList.add("incorrect")
+            }
+        })
+    }
+}
 
-      // TODO 5: Declare an isCorrect function that compares a guess to the right answer
-      // isCorrect(guess) should return true if the guess matches the fact's answer
+startBtn.addEventListener("click",fetchDataAndUpdate)
 
-      function isCorrect(guess) {
-        return guess === fact.answer.toString();
-      }
-
-      // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-
-      for (let button of optionButtons) {
-        button.addEventListener("click", (event) => {
-          // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
-
-          explanation.textContent = fact.explanation;
-
-          // TODO 7: Within the event handler function,
-          // Use a for loop to disable all the option buttons
-
-          for (let disableButton of optionButtons) {
-            disable(disableButton);
-          }
-          // TODO 8: Within the event handler function,
-          // Get the guessed value from the clicked button
-          // Use a conditional to compare the guess to the fact's answer
-          // and add the "correct"/"incorrect" class as appropriate
-          if (isCorrect(button.value)) {
-            button.classList.add("correct");
-          } else {
-            button.classList.add("incorrect");
-          }
-        });
-      }
+async function fetchDataAndUpdate(){
+    counter++
+    try {
+        const newData = await fetchData()
+        GLOBALDATA = newData[counter]
+        startGame(counter+1,newData.length,GLOBALDATA.question)
+        isCorrect(GLOBALDATA.answer)
+        console.log("inside",GLOBALDATA)
+    } catch (error) {
+        console.log("error", error)
+    }
+}
